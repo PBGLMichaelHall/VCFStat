9fa589345d21b8a8ee93642914acf2c812eb2c72
PBGLMichaelHall <michael.safe.browse@gmail.com>
2022-04-14 13:13:59 +0200

    commit

--- R/MH_Functions.R
+++ R/MH_Functions.R
@@ -112,6 +112,45 @@ ChromRO <- function(vcf, chromlist=NULL,windowSize=NULL,binwidth=NULL){
 }
 
 
+#' @title ChromAO
+#' @param vcf A vcf file 
+#' @param chromlist A vector specifying particular chromosomes
+#' @param windowSize Specify window size to calculate number of SNPs
+#' @param binwidth Specify bindwidth for histogram plot
+#' @return A histogram of of Depth FIELD
+#' @examples ChromAO(vcf = "General.vcf", chromlist = c("Chr01","Chr02"),windowsize=1e+05,binwidth=10)
+#' @export ChromAO
+
+ChromAO <- function(vcf, chromlist=NULL,windowSize=NULL,binwidth=NULL){
+  vcf <- read.vcfR(file = "freebayes_D2.filtered.vcf.gz")
+  vcf <- vcfR2tidy(vcf)
+  SNPset <- vcf
+  SNPset <- Map(as.data.frame, SNPset)
+  SNPset <- rbindlist(SNPset, fill = TRUE)
+  if (!is.null(chromlist)) {
+    message("Preparing Data for Quality Control Plotting and removing the following Chromosomes/Contigs: ", 
+            paste(unique(SNPset$CHROM)[!unique(SNPset$CHROM) %in% 
+                                         chromlist], collapse = ", "))
+    SNPset <- SNPset[SNPset$CHROM %in% chromlist, ]
+    message("Finishing Chromosome Subset")
+  }
+  message("Factoring Chromosome Variable According to Unique Specification")
+  SNPset$CHROM <- factor(SNPset$CHROM, levels = gtools::mixedsort(unique(SNPset$CHROM)))
+  message("Selecting Variable Subset")
+  SNPset <- SNPset %>% dplyr::group_by(CHROM) %>% dplyr::mutate(nSNPs = countSNPs_cpp(POS = POS, windowSize = windowSize))
+  par(mfrow = c(1, 1))
+  message("Making breaks Width")
+  SNPset$AO <- as.numeric(SNPset$AO)
+  breaks <- seq(round(min(SNPset$AO) - 1, 0), round(max(SNPset$AO) + 100, 0), binwidth)
+  jpeg(file="plot3.jpeg")
+  message("Plotting histogram")
+  hist(x = SNPset$AO, breaks = breaks, col = "green", xlab = "AO Quantities", main = "Histogram of AO Quantities")
+  dev.off()
+  z <- hist(x = SNPset$AO, breaks = breaks, col = "green", xlab ="AO Quantities", main ="Histogram of AO Quantities")
+  print(z)
+}
+
+
 #' @title ChromMQM
 #' @param vcf A vcf file 
 #' @param chromlist A vector specifying particular chromosomes
