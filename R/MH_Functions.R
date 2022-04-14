04f8018d23ce80b8dcc1ce80d449e2d99fa3f9e2
PBGLMichaelHall <michael.safe.browse@gmail.com>
2022-04-14 12:10:00 +0200

    commit

--- R/MH_Functions.R
+++ R/MH_Functions.R
@@ -3,12 +3,11 @@
 #' @param chromlist A vector specifying particular chromosomes
 #' @param windowSize Specify window size to calculate number of SNPs
 #' @param binwidth Specify bindwidth for histogram plot
-#' @param FIELD Specify info field to plot
-#' @return A histogram of of FIELD
-#' @examples ChromQuality(vcf = "General.vcf", chromlist = c("Chr1", "Chr2")), windowSize = 1e+06, scalar = 0.1, ncol = 2,HighLimQuality = 6000,  binwidth1 = 100, binwidth2 =1, DPBINS=10, p1=TRUE, p2=FALSE, p3=TRUE, p4=TRUE, p5=FALSE, p6=TRUE)
+#' @return A histogram of of Quality FIELD
+#' @examples ChromQuality(vcf = "General.vcf", chromlist = c("Chr01","Chr02"),windowsize=1e+05,binwidth=10)
 #' @export ChromQual
 
-ChromQual <- function(vcf, chromlist=NULL,windowSize=NULL,binwidth=NULL,FIELD=NULL){
+ChromQual <- function(vcf, chromlist=NULL,windowSize=NULL,binwidth=NULL){
 vcf <- read.vcfR(file = "freebayes_D2.filtered.vcf.gz")
 vcf <- vcfR2tidy(vcf)
 SNPset <- vcf
@@ -30,8 +29,45 @@ message("Making breaks Width")
 breaks <- seq(round(min(SNPset$QUAL) - 1, 0), round(max(SNPset$QUAL) + 100, 0), binwidth)
 jpeg(file="plot1.jpeg")
 message("Plotting histogram")
-hist(x = SNPset$QUAL, breaks = breaks, col = "green", xlab = paste0(FIELD,"Quantities"), main = paste0("Histogram of",FIELD))
+hist(x = SNPset$QUAL, breaks = breaks, col = "green", xlab ="Quality Quantities", main = "Histogram of Quality Quantities")
      dev.off()
-z <- hist(x = SNPset$QUAL, breaks = breaks, col = "green", xlab = paste0(FIELD,"Quantities"), main = paste0("Histogram of",FIELD))
+z <- hist(x = SNPset$QUAL, breaks = breaks, col = "green", xlab = "Quality Quantities", main ="Histogram of Quality Quantities")
 print(z)
+}
+
+#' @title ChromQual
+#' @param vcf A vcf file 
+#' @param chromlist A vector specifying particular chromosomes
+#' @param windowSize Specify window size to calculate number of SNPs
+#' @param binwidth Specify bindwidth for histogram plot
+#' @return A histogram of of Depth FIELD
+#' @examples ChromQuality(vcf = "General.vcf", chromlist = c("Chr01","Chr02"),windowsize=1e+05,binwidth=10)
+#' @export ChromQual
+
+ChromDP <- function(vcf, chromlist=NULL,windowSize=NULL,binwidth=NULL){
+  vcf <- read.vcfR(file = "freebayes_D2.filtered.vcf.gz")
+  vcf <- vcfR2tidy(vcf)
+  SNPset <- vcf
+  SNPset <- Map(as.data.frame, SNPset)
+  SNPset <- rbindlist(SNPset, fill = TRUE)
+  if (!is.null(chromlist)) {
+    message("Preparing Data for Quality Control Plotting and removing the following Chromosomes/Contigs: ", 
+            paste(unique(SNPset$CHROM)[!unique(SNPset$CHROM) %in% 
+                                         chromlist], collapse = ", "))
+    SNPset <- SNPset[SNPset$CHROM %in% chromlist, ]
+    message("Finishing Chromosome Subset")
+  }
+  message("Factoring Chromosome Variable According to Unique Specification")
+  SNPset$CHROM <- factor(SNPset$CHROM, levels = gtools::mixedsort(unique(SNPset$CHROM)))
+  message("Selecting Variable Subset")
+  SNPset <- SNPset %>% dplyr::group_by(CHROM) %>% dplyr::mutate(nSNPs = countSNPs_cpp(POS = POS, windowSize = windowSize))
+  par(mfrow = c(1, 1))
+  message("Making breaks Width")
+  breaks <- seq(round(min(SNPset$DP) - 1, 0), round(max(SNPset$QUAL) + 100, 0), binwidth)
+  jpeg(file="plot1.jpeg")
+  message("Plotting histogram")
+  hist(x = SNPset$DP, breaks = breaks, col = "green", xlab = "Depth Quantities", main = "Histogram of Depth Quantities")
+  dev.off()
+  z <- hist(x = SNPset$DP, breaks = breaks, col = "green", xlab ="Depth Quantities", main ="Histogram of Depth Quantities")
+  print(z)
 }
